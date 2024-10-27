7e96c70e2d09f760b115ad83f699cf32fd97e4f4
Umut <116753183+UmutBoyludag@users.noreply.github.com>
2024-10-27 22:20:09 +0300

    last

--- reactlearn/src/components/Pizza.jsx
+++ reactlearn/src/components/Pizza.jsx
@@ -1,12 +1,15 @@
-export default function Pizza() {
+export default function Pizza(props) {
+  
+  console.log(props);
+
     return (
       <div className='col'>
         <div className="card">
-          <img src="/img/pizza1.jpg" alt=""  className='card-img-top p-2 p-md-3 border-bottom'/>
+          <img src= {props.image} alt=""  className='card-img-top p-2 p-md-3 border-bottom'/>
           <div className="card-body">
-            <h2 className='card-title'>Sucuklu Pizza</h2>
-            <p className='card-text'>Lorem ipsum dolor sit amet.</p>
-            <span className='badge text-bg-primary'>200 TL</span>
+            <h2 className='card-title'> {props.title} </h2>
+            <p className='card-text'>{props.description}</p>
+            <span className='badge text-bg-primary'>{props.price} TL</span>
           </div>
         </div>
       </div>
--- reactlearn/src/components/PizzaList.jsx
+++ reactlearn/src/components/PizzaList.jsx
@@ -1,20 +1,23 @@
 import Pizza from "./Pizza";
 
 export default function PizzaList() {
+
+
+  const pizzaObj = {
+    title : "Karışık Pizza",
+    description :  "Süper nefis pizza",
+    image : "/img/pizza1.jpg",
+    price : 300
+  };
+    
+
+
     return (
       <div className='pizza-list'>
         <h2>Pizza List</h2>
         <div className="row row-cols-2 row-cols-md-3 row-cols-xl-4 g-4">
-          <Pizza />
-          <Pizza />
-          <Pizza />
-          <Pizza />
-          <Pizza />
-          <Pizza />
-          <Pizza />
-          <Pizza />
-          <Pizza />
-          <Pizza />
+          <Pizza title= "pizza 1"  description="Süper nefis pizza" image="/img/pizza1.jpg" price="300"/>
+          <Pizza title= "pizza 2"  description="Süper nefis pizza" image="/img/pizza1.jpg" price="400"/>
         </div>
       </div>
     );
